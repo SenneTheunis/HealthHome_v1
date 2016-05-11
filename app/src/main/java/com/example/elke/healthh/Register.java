4b581576352b7614d327795303096aafdbb13df3
Scelke <elkeschoenmaeker@hotmail.com>
2016-05-11 14:02:06 +0200

    databaseConnection
    On the main screen, you can see your username appear

--- app/src/main/java/com/example/elke/healthh/Register.java
+++ app/src/main/java/com/example/elke/healthh/Register.java
@@ -1,22 +1,27 @@
 package com.example.elke.healthh;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 
 public class Register extends AppCompatActivity implements View.OnClickListener{
     Button bRegister;
-    EditText etName,etBirthday, etUsername, etPassword;
+    EditText etName,etBirthday, etUsername, etPassword,etMail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +31,7 @@ public class Register extends AppCompatActivity implements View.OnClickListener{
         etBirthday = (EditText) findViewById(R.id.etBirthday);
         etPassword = (EditText) findViewById(R.id.etPassword);
         etUsername = (EditText) findViewById(R.id.etUsername);
+        etMail=(EditText) findViewById(R.id.etMail);
         bRegister = (Button) findViewById(R.id.bRegister);
 
         bRegister.setOnClickListener(this);
@@ -39,6 +45,7 @@ public class Register extends AppCompatActivity implements View.OnClickListener{
                 String username = etUsername.getText().toString();
                 String password = etPassword.getText().toString();
                 String birthdayText = etBirthday.getText().toString();
+                String email = etMail.getText().toString();
 
                 if(name.length()==0)
                 {
@@ -58,16 +65,80 @@ public class Register extends AppCompatActivity implements View.OnClickListener{
                 {
                     etPassword.requestFocus();
                     etPassword.setError("Please don't leave the password field empty.");
+                }else if(password.length()==0)
+                {
+                    etPassword.requestFocus();
+                    etPassword.setError("Please don't leave the password field empty.");
                 }
+
                 else{
 
                     Toast.makeText(getApplicationContext(), "Registration success",
                             Toast.LENGTH_SHORT).show();
+
+                    BackgroundTask backgroundTask=new BackgroundTask();
+                    backgroundTask.execute(name,username,password,birthdayText,email);
+                    finish();
                     startActivity(new Intent(this,Login.class));
                     break;
                 }
                 break;
         }
     }
+    class BackgroundTask extends AsyncTask<String,Void,String>
+    {
+        String add_info_url;
+        @Override
+        protected void onPreExecute() {
+            add_info_url="http://healthdata.netau.net/dataupload.php";
+            super.onPreExecute();
+        }
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
 
+
+        protected void onPostExecute(String result) {
+            Toast.makeText(getApplicationContext(),result,Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        protected String doInBackground(String... args) {
+            String name,username,password,birthdayText,email;
+            name=args[0];
+            username=args[1];
+            password=args[2];
+            birthdayText=args[3];
+            email=args[4];
+
+            try {
+                URL url=new URL(add_info_url);
+                HttpURLConnection httpURLConnection=(HttpURLConnection)url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                OutputStream outputStream =httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter =new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
+                String data_string = URLEncoder.encode("account", "UTF-8") + "=" + URLEncoder.encode( username, "UTF-8") + "&" +
+                        URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8") + "&" +
+                        URLEncoder.encode("first_name", "UTF-8") + "=" + URLEncoder.encode(name, "UTF-8")+"&" +
+                        URLEncoder.encode("birthday", "UTF-8") + "=" + URLEncoder.encode( birthdayText, "UTF-8")+"&" +
+                        URLEncoder.encode(" email", "UTF-8") + "=" + URLEncoder.encode( email, "UTF-8");
+                bufferedWriter.write(data_string);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream=httpURLConnection.getInputStream();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return "Successful!!";
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
 }
